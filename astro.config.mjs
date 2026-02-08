ddfccdcfef49138d9d9e0f3ad9bf29b7d8c83c42
sidonweb <heysid88@gmail.com>
2026-02-08 23:03:02 +0530

    chore: Add trailing slashes to the site URL and Vite path aliases in `astro.config.mjs`.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,13 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://uiuxwithshree.com",
+  site: "https://uiuxwithshree.com/",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
   vite: {
     resolve: {
       alias: {
-        "@components": fileURLToPath(new URL("./src/components", import.meta.url)),
-        "@layouts": fileURLToPath(new URL("./src/layouts", import.meta.url)),
+        "@components": fileURLToPath(new URL("./src/components/", import.meta.url)),
+        "@layouts": fileURLToPath(new URL("./src/layouts/", import.meta.url)),
       }
 
     },
